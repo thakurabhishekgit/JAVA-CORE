3ea463428a5da7e451fa04936710b47e8d54a85e
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2025-05-29 15:20:39 +0530

    some dsa questions

--- Arrays/PracticeQuestions.java
+++ Arrays/PracticeQuestions.java
@@ -1,4 +1,4 @@
-// import java.util.*;
+import java.util.*;
 // import java.util.stream.Collectors;
 
 public class PracticeQuestions {
@@ -20,6 +20,36 @@ public class PracticeQuestions {
         // arr.stream().filter(x -> x % 2 == 0).forEach(System.out::println);
         // arr.stream().filter(x -> x % 2 != 0).forEach(System.out::println);
 
+        // String s = "aababcabc";
+        // Set<String> set = new HashSet<>();
+        // int count = 0;
+
+        // for (int i = 0; i < s.length() - 2; i++) {
+        // String triplet = s.substring(i, i + 3);
+        // set.clear(); // clear set for every window
+        // for (char c : triplet.toCharArray()) {
+        // set.add(String.valueOf(c));
+        // }
+        // if (set.size() == 3) {
+        // count++;
+        // }
+        // }
+
+        // System.out.println("Count of distinct triplets: " + count);
+
+        //
+
+        int[] nums = { 1, 12, -5, -6, 50, 3 };
+        int k = 4;
+        double maxSumAvg = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length - k + 1; i++) {
+            int[] subarray = Arrays.copyOfRange(nums, i, i + k);
+            double average = Arrays.stream(subarray).average().orElse(0.0);
+            System.out.printf("Average of subarray %s: %.2f%n", Arrays.toString(subarray), average);
+            maxSumAvg = Math.max(maxSumAvg, average);
+
+        }
+        System.out.println("Maximum average of any " + k + " consecutive elements: " + maxSumAvg);
     }
 
 }
