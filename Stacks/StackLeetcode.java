3010eb380663110515457b4450373e05258529ee
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2025-06-04 17:31:33 +0530

    some stacks

---
+++ Stacks/StackLeetcode.java
@@ -0,0 +1,12 @@
+package Stacks;
+
+import java.util.*;
+
+public class StackLeetcode {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        System.out.println("Stack after pushing 1 and 2: " + stack);
+    }
+}
