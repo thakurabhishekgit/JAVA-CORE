410486ada41a2b16520e5ea424823d9fcd7cf44a
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2025-06-04 17:36:49 +0530

    stack parenthesis checker

--- Stacks/StackLeetcode.java
+++ Stacks/StackLeetcode.java
@@ -4,9 +4,25 @@ import java.util.*;
 
 public class StackLeetcode {
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(1);
-        stack.push(2);
-        System.out.println("Stack after pushing 1 and 2: " + stack);
+        Stack<Character> stack = new Stack<>();
+        String s = "([]})";
+        Map<Character, Character> map = new HashMap<>(
+                Map.of(
+                        ')', '(',
+                        '}', '{',
+                        ']', '['));
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                if (!stack.isEmpty() && stack.peek() == map.get(c)) {
+                    stack.pop();
+                } else {
+                    System.out.println("Not balanced");
+                    return;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        System.out.println("Balanced");
     }
 }
