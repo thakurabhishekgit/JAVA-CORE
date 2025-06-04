d9f74bc9693932a54cb3c65270dcca704a7c6731
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2025-06-04 18:18:11 +0530

    eod

--- Stacks/StackLeetcode.java
+++ Stacks/StackLeetcode.java
@@ -3,9 +3,9 @@ package Stacks;
 import java.util.*;
 
 public class StackLeetcode {
-    public static void main(String[] args) {
+
+    public static boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        String s = "([]})";
         Map<Character, Character> map = new HashMap<>(
                 Map.of(
                         ')', '(',
@@ -17,12 +17,60 @@ public class StackLeetcode {
                     stack.pop();
                 } else {
                     System.out.println("Not balanced");
-                    return;
+                    return false;
                 }
             } else {
                 stack.push(c);
             }
         }
-        System.out.println("Balanced");
+        if (stack.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static int maxDepth(String s) {
+        Stack<Character> stack = new Stack<>();
+        int maxDepth = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+                maxDepth = Math.max(maxDepth, stack.size());
+            } else if (c == ')') {
+                stack.pop();
+            }
+        }
+        return maxDepth;
+    }
+
+    public static int minAddToMakeValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>(
+                Map.of(
+                        ')', '(',
+                        '}', '{',
+                        ']', '['));
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                if (!stack.isEmpty() && stack.peek() == map.get(c)) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.size();
+    }
+
+    public static void main(String[] args) {
+
+        // System.out.println(isValid("()"));
+        // System.out.println(maxDepth("(1+(2*3)+((8)/4))+1"));
+        System.out.println(minAddToMakeValid("(()))))"));
+
     }
 }
