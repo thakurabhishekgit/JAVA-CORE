2aac9d1e27e95341e4121a3c1589a98d6b1e31ee
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2025-12-16 19:02:51 +0530

    Add ArraysAndStringsAPI class with array operations and Stream API examples

---
+++ SteramAPI/ArraysAndStringsAPIMethods.java
@@ -0,0 +1,32 @@
+package SteramAPI;
+
+import java.util.*;
+import java.util.stream.*;
+public class ArraysAndStringsAPI {
+    public static void main(String[] args) {
+
+        int[] arr = { 5, 2, 9, 1, 5, 6 };
+        System.out.println("\nOriginal array: " + Arrays.toString(arr));
+        System.out.println(" sum of array:" + Arrays.stream(arr).sum());
+        System.out.println("Length of array: " + arr.length);
+        Arrays.stream(arr).forEach(System.out::println);
+
+        System.out.println("Max: " + Arrays.stream(arr).max().getAsInt());
+        System.out.println("Min: " + Arrays.stream(arr).min().getAsInt());
+        System.out.println("Average: " + (int) Arrays.stream(arr).average().orElse(0));
+        System.out.printf("Average: %.1f\n", Arrays.stream(arr).average().orElse(0.0));
+        System.out.println("Count: " + Arrays.stream(arr).count());
+        System.out.println("Sum: " + Arrays.stream(arr).sum());
+        System.out.println("Sorted: " + Arrays.stream(arr).sorted().boxed().toList());
+        System.out.println("Distinct: " + Arrays.stream(arr).distinct().boxed().toList());
+        System.out.println("Reversed: " + Arrays.stream(arr).boxed().sorted(Collections.reverseOrder()).toList());
+        Arrays.sort(arr);
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+
+//        int arr[] = {1,4,6,7,8,8};
+//        Arrays.stream(arr).
+//                filter(x -> x % 2 == 0).
+//                map(x -> x * x).
+//                forEach(System.out::println);
+    }
+}
---
+++ SteramAPI/StreamAPIQuestions.java
@@ -0,0 +1,4 @@
+package SteramAPI;
+
+public class StreamAPIQuestions {
+}
