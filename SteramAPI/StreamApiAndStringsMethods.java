05b429d76e8f5aa2715d011b0929a30a0c14d6f9
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2025-12-17 16:20:37 +0530

    working with string array and list

--- SteramAPI/StreamApiAndStringsMethods.java
+++ SteramAPI/StreamApiAndStringsMethods.java
@@ -7,6 +7,17 @@ public class StreamApiAndStringsMethods {
         s.chars()
                 .forEach(System.out::println);
 
+        s.chars()
+                .mapToObj(c -> (char)c)
+                .forEach(System.out::println);
+
+
+        String str =  "java stream api";
+//        String[] arr = str.split(" ");
+//        System.out.println(Arrays.toString(arr));
+        Arrays.stream(str.split(" "))
+                .filter(c -> c.startsWith("j"))
+                .forEach(System.out::println);
 
     }
 }
