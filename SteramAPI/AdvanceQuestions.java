da5f640fcca9c64cf2a59321f8e8e6b00693c3c5
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2025-12-17 15:29:28 +0530

    making a new file AdvanceQuestions.java for stream  + hashmap questions and working on printing value pair and entrySet() quetsions

---
+++ SteramAPI/AdvanceQuestions.java
@@ -0,0 +1,46 @@
+package SteramAPI;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class AdvanceQuestions {
+    public static void main(String[] args) {
+
+        //Stream api with Hashmap
+        // print key value pair
+
+        List<Integer> nums = Arrays.asList(1,2,3,4,12,1,2,3,4,5,2,7,5,5 ,3 ,3);
+        //just printing
+        nums.stream()
+                .collect(Collectors.groupingBy(
+                        x -> x,
+                        Collectors.counting()
+                ))
+                .forEach((key , value) ->
+                        System.out.println("Key : " + key +" Value : " + value));
+
+        //stroing in a map
+
+        Map<Integer , Long> map =
+                nums.stream()
+                        .collect(Collectors.groupingBy(
+                                x -> x,
+                                Collectors.counting()
+                        ));
+        map.forEach((key , value) ->
+                System.out.println("Key : " + key +" Value : " + value));
+
+
+        int maxFreq = nums.stream()
+                .collect(Collectors.groupingBy(
+                        x ->x ,
+                        Collectors.counting()
+                ))
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .get()
+                .getKey();
+
+        System.out.println("maxFreq : " + maxFreq);
+    }
+}
