f2e3611fa70927b4fec1686d8659540d0a3fbc18
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2025-12-17 16:12:57 +0530

    creating StreamApiAndStringsMethods.java for working with Strings and stream api quetsions

---
+++ SteramAPI/StreamApiAndStringsMethods.java
@@ -0,0 +1,12 @@
+package SteramAPI;
+import java.util.*;
+public class StreamApiAndStringsMethods {
+    public static void main(String[] args) {
+        String s = "Hello World";
+        //print ascii values
+        s.chars()
+                .forEach(System.out::println);
+
+
+    }
+}
