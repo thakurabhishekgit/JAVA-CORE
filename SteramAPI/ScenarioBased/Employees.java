5ebb04ca53431f338db86aa1a06f72b8975e7243
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2025-12-20 22:34:32 +0530

    minDeletionSize daily leetcode Question

--- SteramAPI/ScenarioBased/Employees.java
+++ SteramAPI/ScenarioBased/Employees.java
@@ -174,6 +174,8 @@ public class Employees {
 
 
 
+
+
     }
 
 }
--- TUF/StriverSheetCodes.java
+++ TUF/StriverSheetCodes.java
@@ -23,10 +23,27 @@ public class StriverSheetCodes {
         return maxP;
 
     }
+
+    public static int minDeletionSize(String[] strs) {
+        int count = 0;
+        for (int i = 0 ; i < strs[0].length() ; i ++ ) {
+            for (int j = 0; j < strs.length - 1; j ++ ) {
+                if( strs[j].charAt(i) > strs[j + 1].charAt(i)) {
+                    count += 1;
+                    break;
+                }
+            }
+        }
+
+        return count;
+
+    }
     public static void main(String[] args) {
-        int nums[] = { 2,3,-2,4};
-        System.out.println(maxProduct((nums)));
+//        int nums[] = { 2,3,-2,4};
+//        System.out.println(maxProduct((nums)));
         //a
+        String[] strs = {"abc", "bce", "cae"};
+        System.out.println(minDeletionSize(strs));
 
     }
 }
