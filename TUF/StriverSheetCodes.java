ef95bf86bc9e2c4e4692eed23c1474cc6d49fd3f
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2025-12-21 16:26:42 +0530

    minDeletionSize daily leetcode Question

--- TUF/StriverSheetCodes.java
+++ TUF/StriverSheetCodes.java
@@ -38,12 +38,51 @@ public class StriverSheetCodes {
         return count;
 
     }
+
+    public static int minDeletionSize2(String[] strs) {
+        int n = strs.length;
+        int m = strs[0].length();
+        char[][] a = new char[n][];
+        for (int i = 0; i < n; i++) {
+            a[i] = strs[i].toCharArray();
+        }
+        boolean[] resolved = new boolean[n - 1];
+        int unresolved = n - 1;
+
+        int deletions = 0;
+
+        for (int col = 0; col < m && unresolved > 0; col++) {
+            boolean needDelete = false;
+
+            for (int row = 0; row < n - 1; row++) {
+                if (!resolved[row] && a[row][col] > a[row + 1][col]) {
+                    needDelete = true;
+                    break;
+                }
+            }
+
+            if (needDelete) {
+                deletions++;
+                continue;
+            }
+
+            for (int row = 0; row < n - 1; row++) {
+                if (!resolved[row] && a[row][col] < a[row + 1][col]) {
+                    resolved[row] = true;
+                    unresolved--;
+                }
+            }
+        }
+
+        return deletions;
+    }
     public static void main(String[] args) {
 //        int nums[] = { 2,3,-2,4};
 //        System.out.println(maxProduct((nums)));
         //a
         String[] strs = {"abc", "bce", "cae"};
         System.out.println(minDeletionSize(strs));
+        System.out.println(minDeletionSize2(strs));
 
     }
 }
