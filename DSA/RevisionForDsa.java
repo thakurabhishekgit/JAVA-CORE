6d9727886a833930b264d2318732c57f44b17990
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2026-02-17 22:14:02 +0530

    SubArray with max sum

--- DSA/RevisionForDsa.java
+++ DSA/RevisionForDsa.java
@@ -99,4 +99,24 @@ public class RevisionForDsa {
         return -1;
     }
 
+    public static int subarraySum(int[] nums, int k) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1); // important base case
+
+        int prefixSum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            prefixSum += num;
+
+            if (map.containsKey(prefixSum - k)) {
+                count += map.get(prefixSum - k);
+            }
+
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+        }
+
+        return count;
+    }
 }
