dd1f3792d1faf471fe32539c9873dce13d8aa0ce
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2026-02-17 20:44:57 +0530

    revising basic of list and arrays

--- DSA/RevisionForDsa.java
+++ DSA/RevisionForDsa.java
@@ -1,18 +1,31 @@
 package DSA;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class RevisionForDsa {
 
     public static void main(String[] args) {
 
         int arr[] = { 10, 23, 4, 3, 2, 45, 3 };
+        Integer arrI[] = { 10, 23, 4, 3, 2, 45, 3 };
         // Arrays.sort(arr);
         // System.out.println(Arrays.toString(arr));
 
         // sort in decsending order
+        Arrays.sort(arrI, (a, b) -> b - a);
+        System.out.println(Arrays.toString(arrI));
 
-        Arrays.sort(arr, (a, b) -> b - a);
-        System.out.println(Arrays.toString(arr));
+        List<Integer> list = Arrays.asList(arrI);
+
+        list.sort((a, b) -> a - b);
+        System.out.println(list);
+
+        list.contains(10);
+
+        // get index of element
+        list.indexOf(10);
+
+        // sum of all elements
+
     }
 }
