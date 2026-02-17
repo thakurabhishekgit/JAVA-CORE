13d0b258104d7301c9c0c4a5b5b1b77b37d114ca
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2026-02-17 22:29:40 +0530

    209. Minimum Size Subarray Sum

--- DSA/RevisionForDsa.java
+++ DSA/RevisionForDsa.java
@@ -141,4 +141,24 @@ public class RevisionForDsa {
         return res;
     }
 
+    public int minSubArrayLen(int target, int[] nums) {
+
+        int l = 0;
+        int sum = 0;
+        int res = Integer.MAX_VALUE;
+
+        for (int r = 0; r < nums.length; r++) {
+
+            sum += nums[r];
+
+            while (sum >= target) {
+                res = Math.min(res, r - l + 1);
+                sum -= nums[l];
+                l++;
+            }
+        }
+
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+
 }
