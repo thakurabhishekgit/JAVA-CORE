19a6abae84d37560be6b965a508dc7d16fc38736
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2026-02-17 22:33:33 +0530

    findMaxAverage

--- DSA/RevisionForDsa.java
+++ DSA/RevisionForDsa.java
@@ -163,4 +163,19 @@ public class RevisionForDsa {
         return res == Integer.MAX_VALUE ? 0 : res;
     }
 
+    public static double findMaxAverage(int[] nums, int k) {
+        int windowSum = 0;
+        for (int i = 0; i < k; i++) {
+            windowSum += nums[i];
+        }
+        int maxSum = windowSum;
+
+        for (int i = k; i < nums.length; i++) {
+            windowSum += nums[i] - nums[i - k];
+            maxSum = Math.max(maxSum, windowSum);
+        }
+
+        return (double) maxSum / k;
+    }
+
 }
