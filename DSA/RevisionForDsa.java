3d50dd5eb38a63e49a09801db4ad65417f2d5d8f
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2026-02-17 22:19:47 +0530

    lengthOfLongestSubstring without repeated character

--- DSA/RevisionForDsa.java
+++ DSA/RevisionForDsa.java
@@ -121,4 +121,22 @@ public class RevisionForDsa {
 
         return count;
     }
+
+    public static int lengthOfLongestSubstring(String s) {
+
+        int res = 0;
+        int l = 0;
+        Set<Character> set = new HashSet<>();
+
+        for (int r = 0; r < s.length(); r++) {
+            while (set.contains(s.charAt(r))) {
+                set.remove(s.charAt(l));
+                l += 1;
+            }
+            set.add(s.charAt(r));
+            res = Math.max(res, r - l + 1);
+        }
+        return res;
+    }
+
 }
