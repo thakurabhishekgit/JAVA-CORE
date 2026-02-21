1bb8920ffd1f459197cbeab9299bb1b43daa6ee5
thakurabhishekgit <abhishek.thakur.mainacc@gmail.com>
2026-02-21 20:49:13 +0530

    add isPrime and countPrimeSetBits methods for prime bit counting

--- DSA/BinaryConvertion.java
+++ DSA/BinaryConvertion.java
@@ -11,4 +11,27 @@ public class BinaryConvertion {
         int decimal = Integer.parseInt(binary, 2);
         System.out.println("Decimal representation of " + binary + " is: " + decimal);
     }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1)
+            return false;
+
+        for (int i = 2; i < Math.sqrt(n) + 1; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int countPrimeSetBits(int left, int right) {
+        int count = 0;
+        for (int i = left; i < right + 1; i++) {
+            int setBits = Integer.bitCount(i);
+            if (isPrime(setBits)) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
